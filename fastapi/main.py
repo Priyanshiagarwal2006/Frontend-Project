803a422e6a8c992cc5b88d5b21baef8118f59dc7
Priyanshi agarwal <158217046+Priyanshiagarwal2006@users.noreply.github.com>
2025-08-23 15:56:48 +0530

    Delete fastapi directory

--- fastapi/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def hello():
-    return{'message' : 'hello world'}
-
-@app.get("/about")
-def hello():
-    return{'message' : 'hello world hi hi'}
